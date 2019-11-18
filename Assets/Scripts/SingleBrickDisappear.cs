7b39bea37bd230ece5f8e5d399be7d95b51c1221
mshawa2 <mshawa2@uic.edu>
2019-11-18 16:17:55 -0600

    Brick models added as well as scripts to make single brick disappear

---
+++ Assets/Scripts/SingleBrickDisappear.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleBrickDisappear : MonoBehaviour
+{
+    public GameObject bricks;
+
+    void Start()
+    {
+        bricks = GameObject.Find("brick");
+
+        bricks.active = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        bricks.active = false;
+    }
+
+}
