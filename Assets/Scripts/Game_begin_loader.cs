1f437d7a75421241d10b1e0ff29af3fb450bac42
mshawa2 <mshawa2@uic.edu>
2019-11-20 14:58:17 -0600

    Fixed stuff

---
+++ Assets/Scripts/Game_begin_loader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game_begin_loader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
