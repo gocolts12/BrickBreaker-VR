cf02836331cfa4f4d63d9f4f5ad5d609191db978
Brent Yurek <brentagainagain@gmail.com>
2019-11-22 17:18:43 -0600

    ball now bounces back and forth

---
+++ Assets/Scripts/BallStart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallStart : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        this.transform.Rotate(Random.Range(-30, 30), Random.Range(-30, 30), 0, Space.Self);
+        rb.velocity = new Vector3(0, 0, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
