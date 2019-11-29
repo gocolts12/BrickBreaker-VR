c76f5312cd9cc878ce82c2bf2c4dedc6c5cdd528
gocolts12 <mlederer101@gmail.com>
2019-11-29 16:31:53 -0600

    paddle ball sound
    Added sound for ball-paddle collision

---
+++ Assets/paddle_ball_script.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddle_ball_script : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public GameObject ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (collision.gameObject.name == ball.name)
+        {
+            Debug.Log("ball collision");
+            audioSource.Play();
+        }
+
+    }
+}
